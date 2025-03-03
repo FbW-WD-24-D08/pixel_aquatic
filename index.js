c0dad9b0991c3e26ec4db81a95adcf1f0fe86af8
Cagatay Wilhelm <cagatayttnc@web.de>
2025-03-03 15:06:43 +0100

    stepI

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
+const frames = [   
+    "><(((('>",
+    " ><(((('>",
+    "  ><(((('>",
+    "   ><(((('>",
+    "    ><(((('>",
+    "     ><(((('>",
+    "      ><(((('>"
+];
 
-const image1 = newImage();
-//Options of conversion
-image1.src = '';
-//image1.src = 'folder/image.png';
-//convert image into base64 string!
+let index = 0;
+function animateAquarium(){ 
+    document.querySelector("#aquarium").innerText = frames[index].join("\n"); // Ändert den Text im <div> und .join("\n") zeigt mehrere Zeilen an.
+    index = (index + 1) % frames.length; // Nimmt das nächste Frame (wieder von vorne, wenn am Ende)
+    setTimeout(animateAquarium, 200); // Ruft sich selbst nach 200ms erneut auf
+}
+
+animateAquarium();
