ea91b77930d49f25316e1ab901a979f3d528de08
Cagatay Wilhelm <cagatayttnc@web.de>
2025-03-03 13:39:12 +0100

    stepI

---
+++ index.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+const image1 = newImage();
+//Options of conversion
+image1.src = '';
+//image1.src = 'folder/image.png';
+//convert image into base64 string!
