783a00cf514766db9fcf4da198b441e57cbffc9e
Cagatay Wilhelm <cagatayttnc@web.de>
2025-03-04 11:16:22 +0100

    stepII

--- index.js
+++ index.js
@@ -5,14 +5,26 @@ const frames = [
     "   ><(((('>",
     "    ><(((('>",
     "     ><(((('>",
-    "      ><(((('>"
+    "      ><(((('>",
+    `
+    ~~~~~~~~~~~~~~~~~~~~~
+    ~        ><(((('>    ~
+    ~   ><(((('>         ~
+    ~~~~~~~~~~~~~~~~~~~~~
+    `,
+    `
+    ~~~~~~~~~~~~~~~~~~~~~
+    ~   ><(((('>         ~
+    ~        ><(((('>    ~
+    ~~~~~~~~~~~~~~~~~~~~~
+    `
 ];
 
 let index = 0;
 function animateAquarium(){ 
-    document.querySelector("#aquarium").innerText = frames[index].join("\n"); // Ändert den Text im <div> und .join("\n") zeigt mehrere Zeilen an.
+    document.querySelector("#aquarium").innerText = frames[index]; // Ändert den Text im <div>
     index = (index + 1) % frames.length; // Nimmt das nächste Frame (wieder von vorne, wenn am Ende)
-    setTimeout(animateAquarium, 200); // Ruft sich selbst nach 200ms erneut auf
+    setTimeout(animateAquarium, 300); // Ruft sich selbst nach 200ms erneut auf
 }
 
 animateAquarium();
